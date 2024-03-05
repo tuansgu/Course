6229d5945cf3dcca7b8b9aa5de013eea1bfcd54d
PQDuy <141241165+PQDuy@users.noreply.github.com>
2024-03-05 18:15:15 +0700

    Add files via upload
    new update

--- src/main/java/com/mycompany/course/BLL/StudentGradeBLL.java
+++ src/main/java/com/mycompany/course/BLL/StudentGradeBLL.java
@@ -4,10 +4,67 @@
  */
 package com.mycompany.course.BLL;
 
+import com.mycompany.course.DAL.StudentGradeDAL;
+import com.mycompany.course.DTO.CourseDTO;
+import com.mycompany.course.DTO.PersonDTO;
+import com.mycompany.course.DTO.StudentGradeDTO;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author pc
  */
 public class StudentGradeBLL {
-    
+
+    public static ArrayList<StudentGradeDTO> getAll() throws SQLException {
+        return StudentGradeDAL.getInstance().getAll();
+    }
+
+    public static CourseDTO getTitle(int id) {
+        return StudentGradeDAL.getInstance().getTitle(id);
+    }
+
+    public static PersonDTO getName(int id) {
+        return StudentGradeDAL.getInstance().getName(id);
+    }
+
+    public static boolean addNewStudentGrade(int courseID, int studentID) {
+        
+        return StudentGradeDAL.getInstance().addNewStudentGrade(courseID, studentID);
+    }
+
+    public static boolean updateStudentGrade(StudentGradeDTO studentGradeDTO) {
+        return StudentGradeDAL.getInstance().updateStudentGrade(studentGradeDTO);
+    }
+
+    public static ArrayList<CourseDTO> getAllCourse() throws SQLException {
+        return StudentGradeDAL.getInstance().getAllCourse();
+    }
+
+    public static ArrayList<PersonDTO> getAllStudent() throws SQLException {
+        return StudentGradeDAL.getInstance().getAllStudent();
+    }
+
+    public static CourseDTO getCourseID(String title) {
+        return StudentGradeDAL.getInstance().getCourseID(title);
+    }
+
+    public static PersonDTO getStudentID(String Firstname) {
+        return StudentGradeDAL.getInstance().getStudentID(Firstname);
+    }
+
+    public static List findStudentGrade(String z, String txtSearch) throws SQLException {
+        List list = new ArrayList();
+        list = StudentGradeDAL.getInstance().findStudentGrade(z, txtSearch);
+        return list;
+    }
+
+    public static List findStudentName(String txtSearch) throws SQLException {
+        List list = new ArrayList();
+        list = StudentGradeDAL.getInstance().findStudentName(txtSearch);
+        return list;
+    }
+
 }
